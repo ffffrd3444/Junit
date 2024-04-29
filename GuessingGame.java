dbdd22cf81a7e4a04f5be9cea822dcb48275d799
ffffrd3444 <108931532+ffffrd3444@users.noreply.github.com>
2024-04-29 13:32:13 +0100

    Add files via upload
    Junit Testing

---
+++ GuessingGame.java
@@ -0,0 +1,10 @@
+package com.example.game.game;
+
+import org.testng.annotations.Test;
+
+public class GuessingGame {
+
+    public String guess(int guessedNumber) {
+        return "You got it";
+    }
+}
---
+++ GuessingGameTest.java
@@ -0,0 +1,14 @@
+import com.example.game.game.GuessingGame;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GuessingGameTest {
+
+    @Test
+    public void testSimpleWinSituation(){
+        GuessingGame game= new GuessingGame();
+        String message=game.guess(3);
+        assertEquals("You got it",message);
+    }
+}
